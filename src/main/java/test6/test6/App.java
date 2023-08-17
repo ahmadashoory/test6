bcc1530701f32fa4ea0c07bbb590a283dc9972eb
ahmad ashoory <azashoory@gmail.com>
2023-08-16 18:44:49 -0700

    message

---
+++ src/main/java/test6/test6/App.java
@@ -0,0 +1,13 @@
+package test6.test6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
